43e1ad44a137659d434582bc6997992f6e8b1491
Tamilselvi459 <tamilselvig459@gmail.com>
2023-03-14 21:10:23 +0530

    Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,13 +5,13 @@ public class Main {
     {
 
     Product p = new Product();
-    int ans = p.Product(4, 5);
-    int ans1 = p.Product(2, 3, 4);
+    int ans = p.Product(10, 20);
+    int ans1 = p.Product(10, 20, 30);
     double ans2 = p.Product(10.5, 9.7);
 
     System.out.println(ans +" "+ans1 +" "+ans2);
    }
-    static class Product{
+   public  static class Product{
        public int Product(int x , int y)
        {
            return x*y;
