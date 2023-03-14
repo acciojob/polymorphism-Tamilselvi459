6313610f64a71e366cf125005f10d460ff5c5abd
Tamilselvi459 <tamilselvig459@gmail.com>
2023-03-14 21:36:53 +0530

    assingment added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,25 +5,25 @@ public class Main {
     {
 
     Product p = new Product();
-    int ans = p.Product(10, 20);
-    int ans1 = p.Product(10, 20, 30);
-    double ans2 = p.Product(10.5, 9.7);
+    int ans = p.product(10, 20);
+    int ans1 = p.product(10, 20, 30);
+    double ans2 = p.product(10.5, 9.7);
 
     System.out.println(ans +" "+ans1 +" "+ans2);
    }
-   public  static class Product{
-       public int Product(int x , int y)
-       {
-           return x*y;
-       }
-       public int Product(int x , int y , int z)
-       {
-           return x*y*z;
-       }
-       public double Product(double x , double y)
-       {
-           return x*y;
-       }
+    public  static class Product{
+        public int product(int x , int y)
+        {
+            return x*y;
+        }
+        public int product(int x , int y , int z)
+        {
+            return x*y*z;
+        }
+        public double product(double x , double y)
+        {
+            return x*y;
+        }
     }
 
 }
