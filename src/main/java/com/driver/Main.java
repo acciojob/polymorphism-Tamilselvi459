e318bec2203f0c28166877704c91d0ed7677f4f9
Tamilselvi459 <tamilselvig459@gmail.com>
2023-03-14 20:34:25 +0530

    code add

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,29 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[]args)
+    {
+
+    Product p = new Product();
+    int ans = p.Product(4, 5);
+    int ans1 = p.Product(2, 3, 4);
+    double ans2 = p.Product(10.5, 9.7);
+
+    System.out.println(ans +" "+ans1 +" "+ans2);
+   }
+    static class Product{
+       public int Product(int x , int y)
+       {
+           return x*y;
+       }
+       public int Product(int x , int y , int z)
+       {
+           return x*y*z;
+       }
+       public double Product(double x , double y)
+       {
+           return x*y;
+       }
+    }
 
 }
